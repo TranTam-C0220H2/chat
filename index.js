fda832d9fa50b1b0f2a8bed7d83846a4adeb3aba
tam <tam.tvt.ptit@gmail.com>
2022-01-24 17:20:31 +0700

    update socket id to database

--- index.js
+++ index.js
@@ -3,23 +3,62 @@ const http = require("http");
 var app = express();
 const server = http.createServer(app);
 
+const API_URI = 'http://apichatapp.hanbienc.com/api';
+
 const socketIo = require("socket.io")(server, {
     cors: {
         origin: "*",
     }
-  }); 
+});
 
 socketIo.on("connection", (socket) => { ///Handle khi có connect từ client tới
-  console.log("New client connected" + socket.id); 
+    // import fetch from "node-fetch";
+    console.log("New client connected" + socket.id);
 
-  socket.on("sendDataClient", function(data) { // Handle khi có sự kiện tên là sendDataClient từ phía client
-    socketIo.emit("sendDataServer", { data });// phát sự kiện  có tên sendDataServer cùng với dữ liệu tin nhắn từ phía server
-  })
+    //update socket id to database
+    socket.on('update_socket_id', function (_token, socket_id) {
+        updateSocketId(socket, _token, socket_id);
+    });
 
-  socket.on("disconnect", () => {
-    console.log("Client disconnected"); // Khi client disconnect thì log ra terminal.
-  });
+    socket.on('create_room_group', function (_token, room_key, members) {
+
+    });
+
+    socket.on('join_room', function (room) {
+        console.log('client join room = ' + room)
+        socket.join(room);
+    });
+
+    socket.on("send_data_client", function (data) { // Handle khi có sự kiện tên là sendDataClient từ phía client
+
+        socketIo.emit("send_data_server", {data});// phát sự kiện  có tên sendDataServer cùng với dữ liệu tin nhắn từ phía server
+    })
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected"); // Khi client disconnect thì log ra terminal.
+    });
 });
+
+function updateSocketId(socket, _token, socket_id) {
+    fetch(`${API_URI}/user/update-socket-id`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${_token}`,
+        },
+        body: JSON.stringify({
+            socket_id,
+        }),
+    })
+        .then(res => res.json())
+        .then(function (response) {
+
+        })
+        .catch(err => {
+            socket.to(socket_id).emit('socket_error', err);
+            console.log('Update socket id: ', err);
+        })
+}
 
 server.listen(process.env.PORT || 3000, () => {
     console.log('Server đang chay');
