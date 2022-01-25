a33ecf72e3cc0dbb2bf9b9e49564c0a901ea0661
tam <tam.tvt.ptit@gmail.com>
2022-01-25 10:26:59 +0700

    test send emit 9

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ socketIo.on("connection", (socket) => { ///Handle khi có connect từ client t�
             },
             body: JSON.stringify({ socket_id: data.socket_id }),
         })
-            .then(res => res.text())
+            .then(res => console.log('res_', res))
             .then(function (response) {
                 console.log('response__', response)
             })
